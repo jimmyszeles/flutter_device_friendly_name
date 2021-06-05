cbbc31c2b05e4563f044a23939b725f7d87004b9
Jimmy Szeles <szeles.jimmy@gmail.com>
2021-06-05 23:51:04 +0200

    Migrated to null-safety and update flutterEmbedding to version 2

--- lib/flutter_device_friendly_name.dart
+++ lib/flutter_device_friendly_name.dart
@@ -6,8 +6,8 @@ class FlutterDeviceFriendlyName {
   static const MethodChannel _channel =
       const MethodChannel('flutter_device_friendly_name');
 
-  static Future<String> get friendlyName async {
-    final String version = await _channel.invokeMethod('getDeviceFriendlyName');
+  static Future<String?> get friendlyName async {
+    final String? version = await _channel.invokeMethod('getDeviceFriendlyName');
     return version;
   }
 }
